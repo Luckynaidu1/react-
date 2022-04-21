ecb09f4b3d7606f940dba7a8b3509dca0a5d3843
Shamballi Lakshmi prasanna <93312353+Luckynaidu1@users.noreply.github.com>
2022-04-21 12:37:58 +0530

    Add files via upload

---
+++ courencyconverter/Currencyop.jsx
@@ -0,0 +1,13 @@
+const Currencyop = ({currencyOptions,prop,updateHandler}) => {
+    const Options = currencyOptions.map((data,index)=>{
+      return <option key={index} value={data}>{data}</option>
+    })
+  return (<>
+      <div className='selectOptions mt-3'>
+        <select className='form-select border-0 rounded-0' onChange={updateHandler}>
+          {Options}
+        </select>
+    </div>
+  </>)
+}
+export default Currencyop
